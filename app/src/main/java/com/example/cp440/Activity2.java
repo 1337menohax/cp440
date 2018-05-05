21515d8c72d391c0f4b1d6584ef6348d840586e7
1337menohax <vivu493@gmail.com>
2018-05-05 13:21:42 -0700

    Commit message.

--- app/src/main/java/com/example/cp440/Activity2.java
+++ app/src/main/java/com/example/cp440/Activity2.java
@@ -1,13 +1,27 @@
 package com.example.cp440;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Activity2 extends AppCompatActivity {
-
+    private Button skip;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+        skip = (Button)findViewById(R.id.button2);
+        skip.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                openBattleScreen();
+            }
+        });
     }
+    public void openBattleScreen(){
+        Intent intent = new Intent(this, battleScreen.class);
+        startActivity(intent);
+        }
 }
